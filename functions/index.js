aa32d48069ab34be55046559fcf1e759bfadc5c3
GLENDALE123 <ghfud2000@gmail.com>
2025-11-02 13:53:09 +0900

    사용자 마이그레이션 기능 제거 및 설정 페이지 업데이트

--- functions/index.js
+++ functions/index.js
@@ -32,13 +32,6 @@ const createNotificationHttps = require('./https/createNotification');
 const registerMobileTokenHttps = require('./https/registerMobileToken');
 const getUnreadNotificationsHttps = require('./https/getUnreadNotifications');
 
-// 사용자 마이그레이션 함수들 (독립 피처)
-const userMigrationHttps = require('./user-migration/analyzeUserSync');
-const userMigrationMigrateHttps = require('./user-migration/migrateUserSync');
-const userMigrationDeleteHttps = require('./user-migration/deleteAuthUsers');
-const userMigrationGetInfoHttps = require('./user-migration/getUserAuthInfo');
-const userMigrationRemovePhoneHttps = require('./user-migration/removePhoneNumberFromFirestore');
-
 // 모든 함수들을 export
 module.exports = {
   // 트리거
@@ -51,12 +44,5 @@ module.exports = {
   ...createNotificationHttps,
   ...registerMobileTokenHttps,
   ...getUnreadNotificationsHttps,
-  
-  // 사용자 마이그레이션 함수들 (독립 피처 - 삭제 가능)
-  ...userMigrationHttps,
-  ...userMigrationMigrateHttps,
-  ...userMigrationDeleteHttps,
-  ...userMigrationGetInfoHttps,
-  ...userMigrationRemovePhoneHttps,
 };
 
--- src/features/user-migration/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 사용자 동기화 마이그레이션 피처
- * 독립 피처 - 삭제 가능
- */
-
-export { MigrationPanel } from './components/MigrationPanel';
-export type * from './types';
-
